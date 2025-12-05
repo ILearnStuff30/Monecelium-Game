facf0395dbead2040531738a59bc5851a6d53c43
Stumper <LOUIS.MARSHALL24@bathspa.ac.uk>
2025-12-05 14:11:44 +0000

    Import Unity Project

---
+++ Assets/Scripts/CircleScript.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class CircleScript : MonoBehaviour
+{
+    public GameObject Circle;
+    public int nOfIterations;
+    public float growthScalar;
+    public int sortingOrder;
+    public int moneyValue;
+    [Header("Customisable Values")]
+    public float growthSpeed = 0.025f;
+    public int iterationMax = 100;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnCircle", 0f, growthSpeed);
+        growthScalar = 0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyValue = nOfIterations * 2;
+    }
+
+
+    public void SpawnCircle()
+    {
+        Color lerpedColour = Color.white;
+        nOfIterations++;
+        sortingOrder--;
+        if (nOfIterations < iterationMax)
+        {
+            growthScalar += Random.Range(0.00025f, (0.125f / 2));
+            GameObject CircleInstance = Instantiate(Circle);
+            CircleInstance.transform.parent = this.gameObject.transform;
+            CircleInstance.transform.position = transform.position;
+            CircleInstance.transform.localScale = new Vector2(1f, 1f) * growthScalar;
+            lerpedColour = Color.Lerp(Color.red, Color.white, Mathf.PingPong(Time.time, 1));
+            CircleInstance.GetComponent<SpriteRenderer>().color = lerpedColour;
+            CircleInstance.GetComponent<SpriteRenderer>().sortingOrder = sortingOrder;
+        }
+        else
+        {
+            CancelInvoke("SpawnCircle");
+        }
+    }
+}
---
+++ Assets/Scripts/MoneyScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MoneyScript : MonoBehaviour
+{
+
+    // The currency is called fungi bucks! :3
+    public int moneyTotal;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
